50b421aebf00e1819a2166981123e451f4f3c4f2
Arihant <arihant.jhaa@gmail.com>
2026-09-26 01:16:08 +0530

    Add solution for Power of Two

--- solutions/easy/Power of Two.js
+++ solutions/easy/Power of Two.js
@@ -3,14 +3,4 @@
             // Language: JavaScript
             // Link: https://leetcode.com/problems/power-of-two/
 
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isPowerOfTwo = function (n) {
-    if (n == 1)
-        return true;
-    else if (n < 1 || (n % 2 != 0))
-        return false;
-    return isPowerOfTwo(n / 2);
-};
+/
